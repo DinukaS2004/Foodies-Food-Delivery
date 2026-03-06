5c883f734c0320f509f633df5a339abdae83ec0b
Dinuka Samarasinghe <dinukad735@gmail.com>
2026-03-06 22:41:25 +0530

    Implemented FoodController

---
+++ src/main/java/com/dinukas2004/foodiesapi/controller/FoodController.java
@@ -0,0 +1,39 @@
+package com.dinukas2004.foodiesapi.controller;
+
+import com.dinukas2004.foodiesapi.io.FoodRequest;
+import com.dinukas2004.foodiesapi.io.FoodResponse;
+import com.dinukas2004.foodiesapi.service.FoodService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/foods")
+@AllArgsConstructor
+public class FoodController {
+
+    private final FoodService foodService;
+
+    @PostMapping
+    public FoodResponse addFood(@RequestPart("food") String foodString,
+                                @RequestPart("file") MultipartFile file) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        FoodRequest request = null;
+
+        try {
+            request = objectMapper.readValue(foodString, FoodRequest.class);
+        } catch (JsonProcessingException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid JSON Format");
+        }
+
+        FoodResponse response = foodService.addFood(request, file);
+        return response;
+    }
+}
